2a6693ffb08958754496a770d2c5d00e46c51a84
Simon Burbidge <simon@whiskmobile.com>
2015-10-01 11:47:38 +0800

    Added additionalTopInset property

--- Source/Classes/SLKTextViewController.h
+++ Source/Classes/SLKTextViewController.h
@@ -98,6 +98,9 @@ NS_CLASS_AVAILABLE_IOS(7_0) @interface SLKTextViewController : UIViewController 
 /** YES if the scrollView should scroll to bottom when the keyboard is shown. Default is NO.*/
 @property (nonatomic, assign) BOOL shouldScrollToBottomAfterKeyboardShows;
 
+/** Additional contentInset to be added at the "top" */
+@property (nonatomic, assign) float additionalTopInset;
+
 /**
  YES if the main table view is inverted. Default is YES.
  This allows the table view to start from the bottom like any typical messaging interface.
